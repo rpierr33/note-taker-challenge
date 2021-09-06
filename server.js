2b10f66d4de718d29ab7b46663c9b3d7e5795df8
mrralphpierre <mrralphpierre@gmail.com>
2021-09-06 15:52:36 -0400

    finished server.js

--- server.js
+++ server.js
@@ -34,6 +34,31 @@ app.post('/api/notes', (req, res) => {
     res.json("you have successfully added a note!");
 });
 
-app.get
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.delete("/api/notes/:id", function (req, res) {
+    console.log(uuidv1());
+    console.log("Req.params:", req.params);
+    let deletedNote = parse.Int(req.params.id);
+    console.log(deletedNote);
 
 
+
+for (let i = 0; i < dbJson.length; i++) {
+    if(deletedNote === dbJson[i].id) {
+        dbJson.splice(i, 1);
+
+        let noteJson = JSON.stringify(dbJson, null, 2);
+        writeFileAsync("./db/db.json", noteJson).then(function () {
+            console.log("your note has been deleted!");
+            });
+     }
+    }
+    res.json(dbJson);
+});
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`)
+});
