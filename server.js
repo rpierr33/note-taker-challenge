1f5d1bf2a794dad3d9e0c7eece49711d38a54be8
mrralphpierre <mrralphpierre@gmail.com>
2021-09-06 09:42:54 -0400

    started server.js file

--- server.js
+++ server.js
@@ -6,5 +6,32 @@ const dbJson = require('./db/db.json');
 const app = express();
 const PORT = 4001;
 
+app.use(express.urlencoded({ extended: true }));
+
+app.use(json());
+
+app.use(express.static("public"));
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+    const dataNotes = fs.readfileSync(path.join(__dirname, './db/db.json'), "utf-8");
+    const parseNotes = JSON.parse(dataNotes);
+    res.json(parseNotes);
+
+});
+
+app.post('/api/notes', (req, res) => {
+    const dataNotes = fs.readFileSync(path.join(__dirname, './db/db/json'), "utf-8");
+    const parseNotes = JSON.parse(dataNotes);
+    parseNotes.push(req.body);
+
+    fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(parseNotes), "utf-8");
+    res.json("you have successfully added a note!");
+});
+
+app.get
 
 
