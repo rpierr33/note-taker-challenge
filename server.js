498018577f2ab12449e09082b20746787ca9c587
mrralphpierre <mrralphpierre@gmail.com>
2021-09-06 09:18:42 -0400

    added fs, path, express, necesary files using require

---
+++ server.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const dbJson = require('./db/db.json');
+
+const app = express();
+const PORT = 4001;
+
+
+
