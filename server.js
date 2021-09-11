9eb1e9f907bf2a30038f158c735c02957c082f39
mrralphpierre <mrralphpierre@gmail.com>
2021-09-11 15:57:38 -0400

    fixed deleting function

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
+
+
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const dbJson = require('./db/db.json');
-var uuidv1 = require('uuidv1')
+const uuidv1 = require('uuid/v1')
+
+
 
 const sequelize = require('./config/config')
 
@@ -31,15 +35,24 @@ app.get('/api/notes', (req, res) => {
     });
 });
 
-app.post('/api/notes', (req, res) => {
-    console.log('DID WE SMACK IN!! ???', req.body)
-    fs.readFile(path.join(__dirname, './db/db.json'), "utf-8", function(err, data) {
-        console.log('PREPARSE', data, err)
-        const parseNotes = JSON.parse(data);
-        
+app.post('/api/notes',(req, res) => {
 
-            console.log('POST PARSE!!', parseNotes)
-        parseNotes.push(req.body);
+    let newNote = {id:uuidv1(), title:req.body.text, text:req.body.title};
+
+
+    console.log(req.body)
+    // console.log('DID WE SMACK IN!! ???', req.body)
+    fs.readFile(path.join(__dirname, './db/db.json'), "utf-8", function(err, data) {
+        // console.log('PREPARSE', data, err)
+      
+        // console.log("This is the data id", data);
+       
+        const parseNotes = JSON.parse(data);
+        console.log(parseNotes);
+
+            // console.log('POST PARSE!!', parseNotes)
+        parseNotes.push(newNote);
+        
 
         fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(parseNotes), "utf-8");
         res.json("you have successfully added a note!");
@@ -68,26 +81,49 @@ app.get('*', (req, res) => {
 
 
 app.delete("/api/notes/:id", function (req, res) {
-    // console.log(uuidv1());
-    console.log('DID WE SMACK IN!! ???', req.body)
-    // console.log("Req.params:", req.params);
-    let deletedNote = parse.Int(req.params.id);
-    console.log(deletedNote);
+
+    //Read the 'file
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+        if (err) {
+            throw err;
+          }
+        let parseNotes = JSON.parse(data);
+        const deletedThisNote = parseNotes.findIndex(note =>note.id === req.params.id);
+        parseNotes.splice(deletedThisNote, 1);
+        const output = fs.writeFile("./db/db.json", JSON.stringify(parseNotes), (err) => {
+            if (err) {
+              throw err;
+            }
+            console.log("Note has been deleted");
+          });
+          res.send(output); 
+    });
+});  
+    //Get the note through filter
+    //remove the note
+    //save the file
 
 
 
-for (let i = 0; i < dbJson.length; i++) {
-    if(deletedNote === dbJson[i].id) {
-        dbJson.splice([i], 1);
+//     // console.log(uuidv1());
+//     console.log('DID WE SMACK IN!! ???', req.body)
+//     // console.log("Req.params:", req.params);
+//     let deletedNote = objNew.findIndex((newNote) => note.id === req.params.id);
+//     console.log('deletedNote');
 
-        let noteJson = JSON.stringify(dbJson, null, 2);
-        writeFileAsync("./db/db.json", noteJson).then(function () {
-            console.log("your note has been deleted!");
-            });
-     }
-    }
-    res.json(dbJson);
-});
+
+
+// for (let i = 0; i < dbJson.length; i++) {
+//     if(deletedNote === dbJson[i].id) {
+//         dbJson.splice([i], 1);
+
+//         // let noteJson = JSON.stringify(dbJson, null, 2);
+//         // writeFileAsync("./db/db.json", noteJson).then(function () {
+//         //     console.log("your note has been deleted!");
+//         //     });
+//      }
+//     }
+//     res.json(dbJson);
 
 
     app.listen(PORT, () => {
